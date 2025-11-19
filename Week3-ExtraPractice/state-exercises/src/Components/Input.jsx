9d505c5a7e3190d2c31015c342166328c5eae37f
lienaa <ajhlien91@gmail.com>
2025-11-18 16:28:05 -0800

    extra-exercises: completed input form

--- Week3-ExtraPractice/state-exercises/src/Components/Input.jsx
+++ Week3-ExtraPractice/state-exercises/src/Components/Input.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 export function Input() {
+  const [text, setText] = useState("");
+  const [display, setDisplay] = useState("");
+
   return (
     <div className="py-8">
-      <form>
-        <input type="text" placeholder="enter text" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setDisplay(text);
+          return;
+        }}
+      >
+        <input
+          type="text"
+          placeholder="enter text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <input type="submit" />
       </form>
+      <p>{display}</p>
     </div>
   );
 }
