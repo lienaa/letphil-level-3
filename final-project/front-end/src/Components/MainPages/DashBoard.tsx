2f2e4a0dac1e7255018c7d16f945234be2027da0
lienaa <ajhlien91@gmail.com>
2025-12-14 23:00:57 -0800

    adjusted reroll button

--- final-project/front-end/src/Components/MainPages/DashBoard.tsx
+++ final-project/front-end/src/Components/MainPages/DashBoard.tsx
@@ -3,7 +3,7 @@ import { TopGames } from "../TopGames";
 export const DashBoard = () => {
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 items-center">
         <h1>Top Picks:</h1>
         <button>Reroll</button>
         <TopGames />
